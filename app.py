a7a626c59bb4730b8438c15a6cedf33737a38bd6
Tomiwa-S <timmeysam@gmail.com>
2025-09-19 16:33:31 +0100

    initial

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello from Python Lambda!"
-    }
+# def handler(event, context):
+#     return {
+#         "statusCode": 200,
+#         "body": "Hello from Python Lambda!"
+#     }
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Flask on EC2 with GitHub Actions!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
